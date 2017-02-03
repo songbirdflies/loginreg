5597ee6ef44f737d737461880606977973b84c4d
Song <satthajaroon@gmail.com>
2017-02-03 13:44:47 -0800

    added logout to the navbar, but login endpoint not working.

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -3,6 +3,7 @@ var jwt = require('jwt-simple');
 var moment = require('moment');
 
 module.exports = {
+
 	register: function (req, res){
 		console.log(req.body);
 		
@@ -18,32 +19,35 @@ module.exports = {
 				if(err){
 					res.status(500).send({
 						message: err.message
-					})
+					});
 				}
-				res.status(200).send({token: createToken(result)});
+				res.status(200).send({
+					token: createToken(result)
+				});
 			});
-		})
+		});
 	},
 
 	login: function (req, res){
-		User.findOne({
-			email: req.body.email
+	console.log('testing back-end');
+	
+	User.findOne({ 
+		email: req.body.email 
 		}, function (err, user) {
-			if (!user)
-				return res.status(401).send({
-					message: 'Email or Password invalid'
-				});
-			if(req.body.pwd == user.pwd)
-				res.send({token:createToken(user)
-				});
-			else {
-				return res.status(401).send({
-					message: 'Invalid email and/or password'
-				})
+			
+			if(!user) {
+				return res.status(401).send({ message: 'Email or Password invalid' });
+			}
+			if(req.body.pwd == user.pwd) {
+				console.log(req.body, user.pwd);
+				res.send({ token: createToken(user) });
+			} else {
+				return res.status(401).send({ message: 'Invalid email and/or password'});
 			}
 		});
 	}
-}
+
+};
 
 function createToken(user){
 	var payload = {
--- frontend/src/app/auth/auth.controller.js
+++ frontend/src/app/auth/auth.controller.js
@@ -6,20 +6,20 @@ export class AuthController {
 		this.$auth = $auth;
 	}
 
+	login() {
+		console.log('test');
+		var vm = this;
+
+		this.$auth.login(this.login.user).then(function(token){
+			vm.$auth.setToken(token);
+		});
+	}
+
 	register() {
 		//console.log('test');
 		var vm = this;
 
 		this.$auth.signup(this.user).then(function(token){
-			vm.$auth.setToken(token);
-		});
-	}
-
-		login() {
-		//console.log('test');
-		var vm = this;
-
-		this.$auth.login(this.login.user).then(function(token){
 			vm.$auth.setToken(token);
 		});
 	}
